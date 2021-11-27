26affc24569df5be46ad8140acf2b659f7869167
komalsingh <komalsingh>
2021-11-27 20:08:31 +0530

    My swapping code added

---
+++ SwapWithoutThird.java
@@ -0,0 +1,14 @@
+
+public class SwapWithoutThird {
+
+	public static void main(String[] args) {
+		int a=10,b=20;
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+
+}
